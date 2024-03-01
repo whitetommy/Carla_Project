65540e8905eabe8fc64103d2a2015cfe4e4cb7e7
whitetommy <1228tommy@naver.com>
2024-03-02 01:13:33 +0900

    Sudden Unintended Acceleration Logic

--- situation.py
+++ situation.py
@@ -5,10 +5,6 @@ def simulate_sudden_unintended_acceleration(vehicle, brake_status):
     Simulates a sudden unintended acceleration scenario where the vehicle accelerates
     while the brake is supposedly engaged.
     """
-    global is_sudden_acceleration
-    # 급발진 상황을 활성화
-    is_sudden_acceleration = True
-    
     # Forcefully increase throttle and RPM while keeping the brake engaged
     throttle = 1.0  # Max throttle
     rpm = 5000  # High RPM to simulate acceleration
@@ -17,7 +13,7 @@ def simulate_sudden_unintended_acceleration(vehicle, brake_status):
     control = carla.VehicleControl(throttle=throttle, steer=0.0, brake=brake)
     vehicle.apply_control(control)
 
-    # set red light of brake in the SUA situaiton.
+    # set red light of brake in the sua situaiton.
     vehicle.set_light_state(carla.VehicleLightState(carla.VehicleLightState.Brake))
 
     # Update visual feedback or print statement to indicate SUA is happening
